b24610bb678316fad4070fdd356611644590aa2f
František Gurecký <gurecfra@cvut.cz>
2025-02-22 15:12:32 +0100

    add: Server sends data from stdin

--- code/server.js
+++ code/server.js
@@ -3,10 +3,13 @@ const WebSocketServer = require('ws').Server;
 const wss = new WebSocketServer({ port: 8080 });
 console.log('Server is running on port 8080');
 
+let clientSocket = null;
+
 wss.on('connection', function connection(ws) {
     // A client has connected
     console.log('connected');
-
+    clientSocket = ws;
+    
     // Declare actions for the server
     ws.on('error', console.error);
     
@@ -16,7 +19,15 @@ wss.on('connection', function connection(ws) {
 
     ws.on('close', function close() {
         console.log('disconnected');
+        clientSocket = null;
     });
 
     ws.send('Hello from server to client');
+});
+
+// Read from stdin and send to the connected client
+process.stdin.on('data', function(data) {
+    if (clientSocket) {
+        clientSocket.send(data.toString().trim());
+    }
 });
